8eef9a107e07fa3b1faeae5b2ce46f6ba0eef310
hoangviet <hoangviet1312@gmail.com>
2018-08-01 13:41:22 +0700

    add class Login Logout

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getParameter("islogout") != null) {
+            if (request.getParameter("islogout").equals("ok")) {
+                // huy session
+                request.removeAttribute("username");
+                // chuyen sang sang login
+                response.sendRedirect("login.jsp");
+            }
+        }
+    }
+}
